a991cddb69c1cc5049f8e877ac4e5c2765e4b1d0
WinkyL <winky.rain@gmail.com>
2018-10-12 12:20:22 -0400

    Update Client.h

--- include/Client.h
+++ include/Client.h
@@ -1,72 +1,264 @@
+#include <iostream>
 #include <string>
 #include <map>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <strings.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <vector>
-#include <algorithm>
-#include <functional>
 
-#include "../include/logger.h"
-#include "../include/global.h"
+#include "../include/Client.h"
 #include "../include/common.h"
 
-#define TRUE 1
-#define MSG_SIZE 256
-#define BUFFER_SIZE 256
-#define MAXCMD 500
 using namespace std;
 
-class Client {
-public:
-    string ip;
-    int port;
-    string hostname;
-    vector<info> list;
-    int clientfd;
-    enum clientStatus status;
-    struct sockaddr_in client_addr;
+Client::Client(int portNumber) {
+    port = portNumber;
+    clientfd = 0;
+    status = LOGOUT;
+    //get hostname and ip address
+    hostname = GetHostname();
+    ip = GetIP();
+    bzero(&client_addr, sizeof(client_addr));
+}
 
-    Client(int portNumber);
+void Client::Author() {
+    char *cmd = "AUTHOR";
+    cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
+    cse4589_print_and_log("I, %s, have read and understood the course academic integrity policy.\n", "bilinshi");
+    cse4589_print_and_log("[%s:END]\n", cmd);
+}
 
-    void Author();
+void Client::Ip() {
+    char *cmd = "IP";
+    cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
+    cse4589_print_and_log("IP:%s\n", ip.c_str());
+    cse4589_print_and_log("[%s:END]\n", cmd);
+}
 
-    void Ip();
+void Client::Port() {
+    char *cmd = "PORT";
+    cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
+    cse4589_print_and_log("PORT:%d\n", port);
+    cse4589_print_and_log("[%s:END]\n", cmd);
+}
 
-    void Port();
+void Client::List() {
+    sort(list.begin(), list.end());
+    int length = list.size();
+    char *cmd = "LIST";
+    cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
+    for (int i = 0; i < length; i++) {
+        cse4589_print_and_log("%-5d%-35s%-20s%-8d\n", i + 1, list[i].hostname, list[i].ip, list[i].port);
+    }
+    cse4589_print_and_log("[%s:END]\n", cmd);
+}
 
-    void List();
+void Client::Login(string ip, int serverPort) {
+    if (clientfd == 0) {
+        clientfd = ConnectToHost((char *) ip.data(), serverPort);
+    } else {
+        char *msg = "LOGIN:NULL";
+        if (send(clientfd, msg, strlen(msg), 0)) {
+            cout << "Update Client Status--login" << endl;
+        }
+    }
+    while (1) {
+        char *buffer = (char *) malloc(sizeof(char) * BUFFER_SIZE);
+        memset(buffer, '\0', BUFFER_SIZE);
 
-    void Login(string ip, int serverPort);
+        if (recv(clientfd, buffer, BUFFER_SIZE, 0) >= 0) {
+            if (strstr(buffer, "List")) {
+                //update list
+            } else if (strstr(buffer, "Msg")) {
+                //call Receive()
+            } else if (strstr(buffer, "Done")) {
+                break;
+            } else {
+                perror("Unexpected message");
+                break;
+            }
+        }
+    }
+    char *cmd = "LOGIN";
+    cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
+    cse4589_print_and_log("[%s:END]\n", cmd);
 
-    void Refresh();
 
-    void Send(string ip, int clientPort, char *msg);
+}
 
-    void Boardcast(string message);
+void Client::Refresh() {
+    char *msg = "REFRESH:NULL";
+    if (send(clientfd, msg, strlen(msg), 0)) {
+        cout << "Refresh online clients" << endl;
+    }
+    while (1) {
+        char *buffer = (char *) malloc(sizeof(char) * BUFFER_SIZE);
+        memset(buffer, '\0', BUFFER_SIZE);
 
-    void Block(string ip);
+        if (recv(clientfd, buffer, BUFFER_SIZE, 0) >= 0) {
+            if (strstr(buffer, "List")) {
+                //update list
+            } else if (strstr(buffer, "Done")) {
+                break;
+            } else {
+                perror("Unexpected message");
+                break;
+            }
+        }
+    }
+    char *cmd = "REFRESH";
+    cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
+    cse4589_print_and_log("[%s:END]\n", cmd);
+}
 
-    void Unblock(string ip);
+void Client::Send(string ip, int clientPort, char *msg) {
 
-    void Logout();
+}
 
-    void Exit();
+void Client::Boardcast(string message) {
+    char *msg = "BOARDCAST:";
+    strcat(msg, (char *) message.data());
+    if (send(clientfd, msg, strlen(msg), 0)) {
+        cout << "Boardcast to all" << endl;
+    }
+    char *cmd = "BOARDCAST";
+    cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
+    cse4589_print_and_log("[%s:END]\n", cmd);
+}
 
-    void Received(string ip, string msg);
+void Client::Block(string ip) {
+    char *msg = "BLOCK:";
+    strcat(msg, (char *) ip.data());
+    if (send(clientfd, msg, strlen(msg), 0)) {
+        cout << "Block ip: " << ip << endl;
+    }
+    char *cmd = "BLOCK";
+    cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
+    cse4589_print_and_log("[%s:END]\n", cmd);
+}
 
-    void SendFile(string ip, string filePath);
+void Client::Unblock(string ip) {
+    char *msg = "UNBLOCK:";
+    strcat(msg, (char *) ip.data());
+    if (send(clientfd, msg, strlen(msg), 0)) {
+        cout << "Unblock ip: " << ip << endl;
+    }
+    char *cmd = "UNBLOCK";
+    cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
+    cse4589_print_and_log("[%s:END]\n", cmd);
+}
 
-    void Run();
-    
-    int ConnectToHost(char *server_ip, int server_port);
-};
+void Client::Logout() {
+    char *msg = "LOGOUT:NULL";
+    if (send(clientfd, msg, strlen(msg), 0)) {
+        cout << "Update Client Status--logout" << endl;
+    }
+    char *cmd = "LOGOUT";
+    cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
+    cse4589_print_and_log("[%s:END]\n", cmd);
+}
+
+void Client::Exit() {
+    if (status == LOGIN) {
+        close(clientfd);
+    }
+    char *cmd = "EXIT";
+    cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
+    cse4589_print_and_log("[%s:END]\n", cmd);
+    exit(0);
+}
+
+void Client::Received(string ip, string msg) {
+    char *cmd = "RECEIVED";
+    cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
+    cse4589_print_and_log("msg from:%s\n[msg]:%s\n", ip.c_str(), msg.c_str());
+    cse4589_print_and_log("[%s:END]\n", cmd);
+}
+
+void Client::SendFile(string ip, string filePath) {
+}
+
+
+int Client::ConnectToHost(char *server_ip, int server_port) {
+    int fdsocket, len;
+    struct sockaddr_in remote_server_addr;
+
+    fdsocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (fdsocket < 0)
+        perror("Failed to create socket");
+
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_port =htons(port);
+    client_addr.sin_addr.s_addr = inet_addr((char*)ip.data());
+
+    if (bind(fdsocket, (struct sockaddr *) &client_addr, sizeof(client_addr))< 0){
+        perror("Bind failed!");
+    }
+
+    bzero(&remote_server_addr, sizeof(remote_server_addr));
+    remote_server_addr.sin_family = AF_INET;
+    inet_pton(AF_INET, server_ip, &remote_server_addr.sin_addr);
+    remote_server_addr.sin_port = htons(server_port);
+
+    if (connect(fdsocket, (struct sockaddr *) &remote_server_addr, sizeof(remote_server_addr)) < 0)
+        perror("Connect failed");
+
+    return fdsocket;
+}
+
+void Client::Run() {
+
+    while (1) {
+        string cmdLine;
+        getline(cin, cmdLine);
+        if (cmdLine.size() == 0) {
+            exit(1);
+        } else {
+            const char *sep = " ";
+            char *p;
+            p = strtok((char *) cmdLine.data(), sep);
+            char *cmd = p;
+            vector<char *> params;
+            //read other params
+            while (p) {
+                params.push_back(p);
+                p = strtok(NULL, sep);
+            }
+            cout << cmd << endl;
+            if (status == LOGOUT) {
+                if (strcmp(cmd, "AUTHOR") == 0) {
+                    Author();
+                } else if (strcmp(cmd, "IP") == 0) {
+                    Ip();
+                } else if (strcmp(cmd, "PORT") == 0) {
+                    Port();
+                } else if (strcmp(cmd, "LOGIN") == 0) {
+                    if (params.size() <= 2) {
+                        cout << "Error Input" << endl;
+                    }
+                    Login(string(params[1]), atoi(params[2]));
+
+                } else if (strcmp(cmd, "EXIT") == 0) {
+                    Exit();
+                } else {
+                    perror("Unexpected command");
+                }
+            } else {
+                //cout << "cmd"<< cmd << endl;
+                if (strcmp(cmd, "LIST") == 0) {
+                    List();
+                } else if (strcmp(cmd, "REFRESH") == 0) {
+                    Refresh();
+                } else if (strcmp(cmd, "SEND") == 0) {
+
+                } else if (strcmp(cmd, "BOARDCAST") == 0) {
+
+                } else if (strcmp(cmd, "BLOCK") == 0) {
+
+                } else if (strcmp(cmd, "UNBLOCK") == 0) {
+
+                } else if (strcmp(cmd, "LOGOUT") == 0) {
+
+                } else {
+                    perror("Unexpected command");
+                }
+            }
+        }
+    }
+}
