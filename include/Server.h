affb79a68d70c23f120044f2fa5f64437ba08793
WinkyL <winky.rain@gmail.com>
2018-10-11 20:52:52 -0400

    Update Server.h

--- include/Server.h
+++ include/Server.h
@@ -1,13 +1,24 @@
+#include <string>
+#include <map>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <strings.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <stdio.h>
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/socket.h>
 #include <netinet/in.h>
-#include <string>
-#include <map>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+#include "../include/logger.h"
+#include "../include/global.h"
+#include "../include/common.h"
 #define DEFAULT_PORT 8000
 #define MAXLINE 4096
 
@@ -23,7 +34,13 @@ class Server
 {
   public:
     string ip;
+    string hostname;
     int port;
+    int server_socket, head_socket, selret, sock_index;
+    int fdaccept = 0, caddr_len;
+    struct sockaddr_in server_addr, client_addr;
+    fd_set master_list, watch_list;
+
     map<string, int> list;
 
     Server(int port);
