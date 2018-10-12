c34ef277f9cbb9798d7674cea3a8c7a6825eaefa
WinkyL <winky.rain@gmail.com>
2018-10-12 12:20:45 -0400

    Update Client.h

--- include/Client.h
+++ include/Client.h
@@ -2,263 +2,276 @@
 #include <string>
 #include <map>
 
-#include "../include/Client.h"
-#include "../include/common.h"
+#include "../include/Server.h"
 
 using namespace std;
 
-Client::Client(int portNumber) {
+Server::Server(int portNumber) {
     port = portNumber;
-    clientfd = 0;
-    status = LOGOUT;
-    //get hostname and ip address
     hostname = GetHostname();
     ip = GetIP();
-    bzero(&client_addr, sizeof(client_addr));
+    caddr_len = sizeof(client_addr);
 }
 
-void Client::Author() {
+void Server::Author() {
     char *cmd = "AUTHOR";
     cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
     cse4589_print_and_log("I, %s, have read and understood the course academic integrity policy.\n", "bilinshi");
     cse4589_print_and_log("[%s:END]\n", cmd);
 }
 
-void Client::Ip() {
+void Server::Ip() {
     char *cmd = "IP";
     cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
     cse4589_print_and_log("IP:%s\n", ip.c_str());
     cse4589_print_and_log("[%s:END]\n", cmd);
 }
 
-void Client::Port() {
+void Server::Port() {
     char *cmd = "PORT";
     cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
     cse4589_print_and_log("PORT:%d\n", port);
     cse4589_print_and_log("[%s:END]\n", cmd);
 }
 
-void Client::List() {
-    sort(list.begin(), list.end());
-    int length = list.size();
+void Server::List() {
+    sort(clientList.begin(), clientList.end());
+    int length = clientList.size();
     char *cmd = "LIST";
     cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
     for (int i = 0; i < length; i++) {
-        cse4589_print_and_log("%-5d%-35s%-20s%-8d\n", i + 1, list[i].hostname, list[i].ip, list[i].port);
-    }
-    cse4589_print_and_log("[%s:END]\n", cmd);
-}
-
-void Client::Login(string ip, int serverPort) {
-    if (clientfd == 0) {
-        clientfd = ConnectToHost((char *) ip.data(), serverPort);
-    } else {
-        char *msg = "LOGIN:NULL";
-        if (send(clientfd, msg, strlen(msg), 0)) {
-            cout << "Update Client Status--login" << endl;
+        if (clientList[i].status == LOGIN) {
+            cse4589_print_and_log("%-5d%-35s%-20s%-8d\n", i + 1, clientList[i].hostname, clientList[i].ip,
+                                  clientList[i].port);
         }
     }
-    while (1) {
-        char *buffer = (char *) malloc(sizeof(char) * BUFFER_SIZE);
-        memset(buffer, '\0', BUFFER_SIZE);
-
-        if (recv(clientfd, buffer, BUFFER_SIZE, 0) >= 0) {
-            if (strstr(buffer, "List")) {
-                //update list
-            } else if (strstr(buffer, "Msg")) {
-                //call Receive()
-            } else if (strstr(buffer, "Done")) {
-                break;
-            } else {
-                perror("Unexpected message");
-                break;
-            }
-        }
-    }
-    char *cmd = "LOGIN";
-    cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
-    cse4589_print_and_log("[%s:END]\n", cmd);
-
-
-}
-
-void Client::Refresh() {
-    char *msg = "REFRESH:NULL";
-    if (send(clientfd, msg, strlen(msg), 0)) {
-        cout << "Refresh online clients" << endl;
-    }
-    while (1) {
-        char *buffer = (char *) malloc(sizeof(char) * BUFFER_SIZE);
-        memset(buffer, '\0', BUFFER_SIZE);
-
-        if (recv(clientfd, buffer, BUFFER_SIZE, 0) >= 0) {
-            if (strstr(buffer, "List")) {
-                //update list
-            } else if (strstr(buffer, "Done")) {
-                break;
-            } else {
-                perror("Unexpected message");
-                break;
-            }
-        }
-    }
-    char *cmd = "REFRESH";
-    cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
     cse4589_print_and_log("[%s:END]\n", cmd);
 }
 
-void Client::Send(string ip, int clientPort, char *msg) {
-
-}
-
-void Client::Boardcast(string message) {
-    char *msg = "BOARDCAST:";
-    strcat(msg, (char *) message.data());
-    if (send(clientfd, msg, strlen(msg), 0)) {
-        cout << "Boardcast to all" << endl;
-    }
-    char *cmd = "BOARDCAST";
+void Server::Statistic() {
+    sort(clientList.begin(), clientList.end());
+    char *cmd = "STATISTIC";
     cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
-    cse4589_print_and_log("[%s:END]\n", cmd);
-}
-
-void Client::Block(string ip) {
-    char *msg = "BLOCK:";
-    strcat(msg, (char *) ip.data());
-    if (send(clientfd, msg, strlen(msg), 0)) {
-        cout << "Block ip: " << ip << endl;
-    }
-    char *cmd = "BLOCK";
-    cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
-    cse4589_print_and_log("[%s:END]\n", cmd);
-}
-
-void Client::Unblock(string ip) {
-    char *msg = "UNBLOCK:";
-    strcat(msg, (char *) ip.data());
-    if (send(clientfd, msg, strlen(msg), 0)) {
-        cout << "Unblock ip: " << ip << endl;
-    }
-    char *cmd = "UNBLOCK";
-    cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
-    cse4589_print_and_log("[%s:END]\n", cmd);
-}
-
-void Client::Logout() {
-    char *msg = "LOGOUT:NULL";
-    if (send(clientfd, msg, strlen(msg), 0)) {
-        cout << "Update Client Status--logout" << endl;
-    }
-    char *cmd = "LOGOUT";
-    cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
-    cse4589_print_and_log("[%s:END]\n", cmd);
-}
-
-void Client::Exit() {
-    if (status == LOGIN) {
-        close(clientfd);
-    }
-    char *cmd = "EXIT";
-    cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
-    cse4589_print_and_log("[%s:END]\n", cmd);
-    exit(0);
-}
-
-void Client::Received(string ip, string msg) {
-    char *cmd = "RECEIVED";
-    cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
-    cse4589_print_and_log("msg from:%s\n[msg]:%s\n", ip.c_str(), msg.c_str());
-    cse4589_print_and_log("[%s:END]\n", cmd);
-}
-
-void Client::SendFile(string ip, string filePath) {
-}
-
-
-int Client::ConnectToHost(char *server_ip, int server_port) {
-    int fdsocket, len;
-    struct sockaddr_in remote_server_addr;
-
-    fdsocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (fdsocket < 0)
-        perror("Failed to create socket");
-
-    client_addr.sin_family = AF_INET;
-    client_addr.sin_port =htons(port);
-    client_addr.sin_addr.s_addr = inet_addr((char*)ip.data());
-
-    if (bind(fdsocket, (struct sockaddr *) &client_addr, sizeof(client_addr))< 0){
-        perror("Bind failed!");
-    }
-
-    bzero(&remote_server_addr, sizeof(remote_server_addr));
-    remote_server_addr.sin_family = AF_INET;
-    inet_pton(AF_INET, server_ip, &remote_server_addr.sin_addr);
-    remote_server_addr.sin_port = htons(server_port);
-
-    if (connect(fdsocket, (struct sockaddr *) &remote_server_addr, sizeof(remote_server_addr)) < 0)
-        perror("Connect failed");
-
-    return fdsocket;
-}
-
-void Client::Run() {
-
-    while (1) {
-        string cmdLine;
-        getline(cin, cmdLine);
-        if (cmdLine.size() == 0) {
-            exit(1);
+    int length = clientList.size();
+    for (int i = 0; i < length; i++) {
+        char *clientStatus;
+        if (clientList[i].status == LOGIN) {
+            clientStatus = "logged-in";
         } else {
-            const char *sep = " ";
-            char *p;
-            p = strtok((char *) cmdLine.data(), sep);
-            char *cmd = p;
-            vector<char *> params;
-            //read other params
-            while (p) {
-                params.push_back(p);
-                p = strtok(NULL, sep);
-            }
-            cout << cmd << endl;
-            if (status == LOGOUT) {
-                if (strcmp(cmd, "AUTHOR") == 0) {
-                    Author();
-                } else if (strcmp(cmd, "IP") == 0) {
-                    Ip();
-                } else if (strcmp(cmd, "PORT") == 0) {
-                    Port();
-                } else if (strcmp(cmd, "LOGIN") == 0) {
-                    if (params.size() <= 2) {
-                        cout << "Error Input" << endl;
-                    }
-                    Login(string(params[1]), atoi(params[2]));
-
-                } else if (strcmp(cmd, "EXIT") == 0) {
-                    Exit();
-                } else {
-                    perror("Unexpected command");
-                }
-            } else {
-                //cout << "cmd"<< cmd << endl;
-                if (strcmp(cmd, "LIST") == 0) {
-                    List();
-                } else if (strcmp(cmd, "REFRESH") == 0) {
-                    Refresh();
-                } else if (strcmp(cmd, "SEND") == 0) {
-
-                } else if (strcmp(cmd, "BOARDCAST") == 0) {
-
-                } else if (strcmp(cmd, "BLOCK") == 0) {
-
-                } else if (strcmp(cmd, "UNBLOCK") == 0) {
-
-                } else if (strcmp(cmd, "LOGOUT") == 0) {
-
-                } else {
-                    perror("Unexpected command");
-                }
-            }
+            clientStatus = "logged-out";
         }
+        cse4589_print_and_log("%-5d%-35s%-8d%-8d%-8s\n", i + 1, clientList[i].hostname, clientList[i].send,
+                              clientList[i].receive, clientStatus);
     }
+    cse4589_print_and_log("[%s:END]\n", cmd);
+}
+
+void Server::Blocked(string ip) {
+    char *cmd = "BLOCKED";
+    //judge if ip valid
+
+    map<string, vector<struct info> >::iterator iter;
+    iter = blockList.find(ip);
+    if (iter != blockList.end()) {
+        vector<info> blockClients = iter->second;
+        int length = blockClients.size();
+        sort(blockClients.begin(), blockClients.end());
+        cse4589_print_and_log("[%s:SUCCESS]\n", cmd);
+        for (int i = 0; i < length; i++) {
+            cse4589_print_and_log("%-5d%-35s%-20s%-8d\n", i + 1, blockClients[i].hostname, blockClients[i].ip,
+                                  blockClients[i].port);
+        }
+        cse4589_print_and_log("[%s:END]\n", cmd);
+    } else {
+        CommandFail(cmd);
+    }
+}
+
+void Server::Relay() {
+}
+
+void Server::Run() {
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0)
+        perror("Create socket failed!");
+    bzero(&server_addr, sizeof(server_addr));
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr((char *) ip.data());
+    server_addr.sin_port = htons(port);
+
+    if (bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+        printf("Bind failed!");
+        exit(0);
+    }
+
+    if (listen(server_socket, 10) < 0) {
+        printf("Listen socket error!");
+    }
+
+    FD_ZERO(&master_list);
+    FD_ZERO(&watch_list);
+
+    FD_SET(server_socket, &master_list);
+
+    FD_SET(STDIN, &master_list);
+
+    head_socket = server_socket;
+
+    while (1) {
+        memcpy(&watch_list, &master_list, sizeof(master_list));
+        bzero(&client_addr, sizeof(client_addr));
+        selret = select(head_socket + 1, &watch_list, NULL, NULL, NULL);
+        if (selret < 0)
+            perror("Select error!");
+
+        if (selret > 0) {
+            for (sock_index = 0; sock_index <= head_socket; sock_index++) {
+
+                if (FD_ISSET(sock_index, &watch_list)) {
+                    if (sock_index == 0) {
+                        string cmdLine;
+                        getline(cin, cmdLine);
+                        if (cmdLine.size() == 0) {
+                            exit(-1);
+                        } else {
+                            const char *sep = " ";
+                            char *p;
+                            p = strtok((char *) cmdLine.data(), sep);
+                            char *cmd = p;
+                            vector<char *> params;
+                            //read other params
+                            while (p) {
+                                params.push_back(p);
+                                p = strtok(NULL, sep);
+                            }
+                            if (strcmp(cmd, "AUTHOR") == 0) {
+                                Author();
+                            } else if (strcmp(cmd, "IP") == 0) {
+                                Ip();
+                            } else if (strcmp(cmd, "PORT") == 0) {
+                                Port();
+                            } else if (strcmp(cmd, "LIST") == 0) {
+                                List();
+                            } else if (strcmp(cmd, "STATISTIC") == 0) {
+                                Statistic();
+                            } else if (strcmp(cmd, "BLOCKED") == 0) {
+                                if (params.size() <= 1) {
+                                    cout << "Error Input" << endl;
+                                }
+                                Blocked(string(params[1]));
+                            } else {
+                                perror("Unexpected command");
+                            }
+
+                        }
+                    } else if (sock_index == server_socket) { //new client login
+                        cout << "1"<< endl;
+                        int fdaccept = accept(server_socket, (struct sockaddr *) &client_addr, &caddr_len);
+                        if (fdaccept < 0)
+                            perror("Accept failed.");
+
+                        cout<< "2" << endl;
+
+                        char *clientIp = inet_ntoa(client_addr.sin_addr);
+                        int clientPort = (int) ntohs(client_addr.sin_port);
+                        //cout << "newPort" << clientPort << endl;
+                        string clientHostname = GetClientHostname(clientIp);
+                        struct info newClient;
+                        newClient.hostname = (char *) clientHostname.data();
+                        newClient.ip = clientIp;
+                        newClient.port = clientPort;
+                        newClient.send = 0;
+                        newClient.receive = 0;
+                        newClient.status = LOGIN;
+
+                        clientList.push_back(newClient);
+
+                        //here complement the response
+                        int length = clientList.size();
+                        char msg[255];
+                        for (int i = 0; i < length; i++) {
+                            if (clientList[i].status == LOGIN) {
+                                //char msg[255];
+                                strcpy(msg, "List:");
+                                strcat(msg, clientList[i].hostname);
+                                strcat(msg, ",");
+                                strcat(msg, clientList[i].ip);
+                                strcat(msg, ",");
+                                char portString[10];
+                                //cout << clientList[i].port << endl;
+                                sprintf(portString, "%d", clientList[i].port);
+
+                                strcat(msg, portString);
+                                cout << msg << endl;
+                                if (send(fdaccept, msg, strlen(msg), 0)) {
+                                    cout << "Send online client: " << i + 1 << endl;
+                                }
+                                //free(msg);
+                            }
+                        }
+                        //then respond relay
+
+
+                        //finally send Done
+                        char *responseDone = "Done";
+                        if (send(fdaccept, responseDone, strlen(responseDone), 0)) {
+                            cout << "Response done" << endl;
+                        }
+                        /* Add to watched socket list */
+                        FD_SET(fdaccept, &master_list);
+                        if (fdaccept > head_socket)
+                            head_socket = fdaccept;
+                    }
+                        /* Read from existing clients */
+                    else {
+                        /* Initialize buffer to receive response */
+                        char *buffer = (char *) malloc(sizeof(char) * BUFFER_SIZE);
+                        memset(buffer, '\0', BUFFER_SIZE);
+
+                        if (recv(sock_index, buffer, BUFFER_SIZE, 0) <= 0) {
+                            //clear data of sock_index
+
+
+                            close(sock_index);
+                            /* Remove from watched list */
+                            FD_CLR(sock_index, &master_list);
+                        } else {
+                            //Process incoming data from existing clients here ...
+                            if (strstr(buffer, "LOGIN")) {
+                                //update client status
+                                getpeername(sock_index, (struct sockaddr *) &client_addr, &caddr_len);
+                                char *clientIp = inet_ntoa(client_addr.sin_addr);
+                                int length = clientList.size();
+                                for (int i = 0; i < length; i++) {
+                                    if (strcmp(clientList[i].ip, clientIp) == 0) {
+                                        clientList[i].status = LOGIN;
+                                    }
+                                }
+                            } else if (strstr(buffer, "LOGOUT")) {
+                                //update client status
+                                getpeername(sock_index, (struct sockaddr *) &client_addr, &caddr_len);
+                                char *clientIp = inet_ntoa(client_addr.sin_addr);
+                                int length = clientList.size();
+                                for (int i = 0; i < length; i++) {
+                                    if (strcmp(clientList[i].ip, clientIp) == 0) {
+                                        clientList[i].status = LOGOUT;
+                                    }
+                                }
+                            }
+                        }
+
+                        free(buffer);
+                    }
+
+                }
+
+
+            }
+
+        }
+
+    }
+
+
 }
